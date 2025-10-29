7e8561f401c69d05c380eeff880acab204b14f3e
anaslkhl <anaslkhl123@gmail.com>
2025-10-29 02:42:51 +0100

    fisrt version

---
+++ page.js
@@ -0,0 +1,17 @@
+let b = document.getElementById("button");
+b.addEventListener("click", myFunction);
+
+function myFunction() {
+    window.location.href = 'in.html';
+}
+
+
+
+
+
+// function openForm() {
+//     document.getElementById("myform").style.display = "block";
+// }
+// function closeForm() {
+//     let popup = document.getElementById("myform").style.display = "none";
+// }
